15ef3dc74162d628dd787b3b11e9361cf065f436
Ndzalo <ndalomaluks@gmail.com>
2024-06-04 15:03:07 +0200

    Add project files.

---
+++ Animals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance1
+{
+   public class Animals
+    {
+        public void  Animalscan() {
+            Console.WriteLine("Eat");
+            Console.WriteLine("Move");
+            Console.WriteLine("Drink");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace inheritance1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Animalscan();
+            dog.Bark();
+        }
+    }
+}
